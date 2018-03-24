3e652a567e03c769caa2b77b2d1be7335501cd6e
Octaviano KOUDOGBO <okoudogbo@gmail.com>
2018-03-25 00:04:42 +0100

    ok

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tirageloto
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+          SearchRandomNumber();
+        }
+
+        static void SearchRandomNumber()
+        {
+            Random num = new Random();
+            int[] board = new int[8];
+            for (int i = 1; i < 8; i++)
+            {
+                int randomnumber = num.Next(1, 50); //49 inclus
+                if (!board.Contains(randomnumber))
+                {
+                    board[i] = randomnumber;
+                    Console.WriteLine($"Le tirage numéro {i} est le {board[i]}");
+                }
+            }
+        }
+    }
+}
